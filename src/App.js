b70c6ffa704a5a6893a1bbc1a816c00680e2a7fd
Yasir Zafar <yzyasir@yahoo.com>
2020-11-17 11:13:12 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import UserInfoForm from './components/UserInfoForm'
+
+function App() {
+  return (
+    <div className="App">
+      < UserInfoForm />
+    </div>
+  );
+}
+
+export default App;
